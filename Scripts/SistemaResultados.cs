3421d581c4edc8d7e6cb25d45cffbea31aeb1f86
JuanMoreno1571 <jmoreno@estud.usfq.edu.ec>
2022-06-16 17:43:28 -0500

    Update SistemaResultados.cs

--- Scripts/SistemaResultados.cs
+++ Scripts/SistemaResultados.cs
@@ -2,7 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SistemaResultados : MonoBehaviour {
-//Prueba
+
+[SerializeField]
+GameObject barras;
+
+[SerializeField]
+GameObject lineas;
+
+[SerializeField]
+TMP_Text dinero;
+
+[SerializeField]
+TMP_Text rating;
+
+[SerializeField]
+TMP_Text publicReaction;
+
+private void Start (){
+
 }
+
+private void performancePerEpisode (){
+
+}
+
+private void monthlyPerformance( ){
+
+}
+
+private void estadoJuego(){
+
+}
+
+private void moneyMade( ){}
+
+private void calificar(){
+    
+}
+
